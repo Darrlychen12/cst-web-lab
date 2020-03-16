c29da6047b6d722e2b2628e62efa8dacd01f398e
Darrlychen12 <43010873+Darrlychen12@users.noreply.github.com>
2020-03-16 13:13:17 -0700

    Update database.js

--- util/database.js
+++ util/database.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 
 // connect to a database peoplebook running on your localmachine
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'artistbook',
-    password: 'password'
+    host: 'remotemysql.com',
+    user: 'dKgl5Ssh7t',
+    database: 'dKgl5Ssh7t',
+    password: ' XjoxZkghpO',
+    port: 3306
 });
 
 module.exports = pool.promise();
